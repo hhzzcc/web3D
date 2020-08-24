4a246b60cd9d0c1262fdb38eb41a77b8753736e6
lingmao <lingmao@gaoding.com>
2020-08-24 18:30:50 +0800

    fix: 调整fetch地址

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../views')))
+app.use(express.static(path.resolve(__dirname, '../')))
 
 
 const port = 1234;
