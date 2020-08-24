c2d4b58b5f068153eb6d0e2780c7c8fb98a265f6
lingmao <lingmao@gaoding.com>
2020-08-24 18:35:57 +0800

    fix: 修复fetch线上路径

--- views/web3D/shader/index.js
+++ views/web3D/shader/index.js
@@ -1,9 +1,9 @@
-
+const baseUrl = (location.href.includes('github') ? '/web3D' : '') + '/views/web3D/shader';
 // 获取着色器代码
 export const initShader = async () => {
     const [fragmentShaderText, vertexShaderText] = await Promise.all([
-        fetch('/views/web3D/shader/fragment-shader.glsl').then(res => res.text()),
-        fetch('/views/web3D/shader/vertex-shader.glsl').then(res => res.text()),
+        fetch(baseUrl + '/fragment-shader.glsl').then(res => res.text()),
+        fetch(baseUrl + '/vertex-shader.glsl').then(res => res.text()),
     ]);
 
     return {
